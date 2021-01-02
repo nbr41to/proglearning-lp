23fc543a56b32979da2c62b79941d592bcc512d3
Knob <59198478+nbr41to@users.noreply.github.com>
2021-01-02 21:07:11 +0900

    FadeInBox作った

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image"
 import React from 'react'
+import { FadeInBlock } from "src/components/FadeInBlock"
 
 export default function Home() {
   return (
     <div>
-      <Image src="/progLab.002.png" width={10000} height={5500} />
+      <FadeInBlock>
+        <Image src="/progLab.002.png" width={10000} height={5500} />
+      </FadeInBlock>
+      <FadeInBlock>
+        右から
+      </FadeInBlock>
     </div>
   )
 }
