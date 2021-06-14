f30295b1b1c33790b16ebeb60eeef22040c14e94
Nobuyuki Kobayashi <nbr.41to@gmail.com>
2021-06-14 10:56:03 +0900

    mdxのスタイル

---
+++ src/contents-mdx/mdx.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledHomeSection = styled.div`
+  border-radius: 16px;
+  border: 2px double aquamarine;
+  padding: 16px;
+  margin: 32px;
+  h2 {
+    text-align: center;
+  }
+  p {
+    text-align: center;
+  }
+`;
