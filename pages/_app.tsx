3b1af6dbc25a590c52e93e7af96f86961987c117
Knob <59198478+nbr41to@users.noreply.github.com>
2022-01-05 18:07:25 +0900

    ogp twitterを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,11 @@ import type { AppProps } from 'next/app';
 import usePageViewHook from 'src/lib/usePageViewHook';
 import 'src/styles/globals.css';
 
+const ogTitle = 'progLearning';
+const ogDescription =
+  '「学ぶことで人生に変化を。」`progLearning` は、一歩踏み出したい人のプログラミング学習を支援しております。';
+const ogImage = './site_image.png';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   usePageViewHook();
 
@@ -11,16 +16,21 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <Layout>
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="description"
-          content="「学ぶことで人生に変化を。」`progLearning` は、一歩踏み出したい人のプログラミング学習を支援しております。"
-        />
-        <meta property="og:title" content="progLearning" />
-        <meta
-          property="og:description"
-          content="「学ぶことで人生に変化を。」`progLearning` は、一歩踏み出したい人のプログラミング学習を支援しております。"
-        />
-        <meta property="og:image" content="./site_image.png" />
+        <meta name="description" content={ogDescription} />
+        {/* OGP */}
+        <meta property="og:site_name" content={ogTitle} />
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:description" content={ogDescription} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:locale" content="ja_JP" />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={ogTitle} />
+        <meta name="twitter:description" content={ogDescription} />
+        <meta name="twitter:image" content={ogImage} />
+        <meta name="twitter:url" content="https://proglab.nbr41.com/" />
+        <meta name="twitter:site" content="@Knob_nbr41to" />
+
         <title>progLearning</title>
       </Head>
       <Component {...pageProps} />
